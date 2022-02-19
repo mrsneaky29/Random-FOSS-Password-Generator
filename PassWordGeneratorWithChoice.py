096fe128df16e3a0e312a394ecb3834d291f64ff
mrsneaky29 <84763159+mrsneaky29@users.noreply.github.com>
2022-02-19 16:55:20 +0530

    Update PassWordGeneratorWithChoice.py

--- PassWordGeneratorWithChoice.py
+++ PassWordGeneratorWithChoice.py
@@ -1,11 +1,12 @@
-import string
 import random
+import string
 
 
 alphabets = list(string.ascii_letters)
 digits = list(string.digits)
 special_characters = list("!@#$%^&*()")
 characters = list(string.ascii_letters + string.digits + "!@#$%^&*()")
+
 
 def generate_random_password():
 	length = int(input("Password length: "))
@@ -16,9 +17,11 @@ def generate_random_password():
 
 	characters_count = alphabets_count + digits_count + special_characters_count
 
+
 	if characters_count > length:
 		print("Characters total count is greater than the password length")
 		return
+
 
 
 	password = []
@@ -37,14 +40,17 @@ def generate_random_password():
 	for i in range(special_characters_count):
 		password.append(random.choice(special_characters))
 
-
 	if characters_count < length:
 		random.shuffle(characters)
 		for i in range(length - characters_count):
 			password.append(random.choice(characters))
+			
+
 
 	random.shuffle(password)
+
 
 	print("".join(password))
 
 generate_random_password()
+input("Press Enter to Exit")
